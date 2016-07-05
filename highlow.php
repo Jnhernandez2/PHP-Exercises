9e32c04227aefd0adb1b3d2ad17575d5dade95b9
John Hernandez <johnhernandez@Johns-MacBook-Air.local>
2016-07-05 10:46:25 -0500

    This commit includes some more additions to the highlow game. It now has the capability to take arguments and use them as the parameters for the random number generator. It also has checks to make sure the right number of arguments are given and they are both numeric.

--- highlow.php
+++ highlow.php
@@ -1,31 +1,43 @@
 <?php
 
+if ($argc == 3) {
+	if (is_numeric($argv[1])) {
+		if (is_numeric($argv[2])) {
 
-$magicNumber = mt_rand(1,100);
+			$magicNumber = mt_rand($argv[1],$argv[2]);
 
-fwrite(STDOUT, "Can you guess the magic number?\n");
+			fwrite(STDOUT, "Can you guess the magic number?\n");
 
-$userGuess = fgets(STDIN);
-$guessCount = 0;
+			$userGuess = fgets(STDIN);
+			$guessCount = 1;
 
-while ($userGuess != $magicNumber) {
-	$guessCount++;
-	if ($userGuess > $magicNumber) {
-		fwrite(STDOUT, "Too high! Lower!\n");
-		fwrite(STDOUT, "Can you guess the magic number?\n");
-		$userGuess = fgets(STDIN);
-	} elseif ($userGuess < $magicNumber) {
-		fwrite(STDOUT, "Too low! Higher!\n");
-		fwrite(STDOUT, "Can you guess the magic number?\n");
-		$userGuess = fgets(STDIN);
-	} 
-}
+			while ($userGuess != $magicNumber) {
+				$guessCount++;
+				if ($userGuess > $magicNumber) {
+					fwrite(STDOUT, "Too high! Lower!\n");
+					fwrite(STDOUT, "Can you guess the magic number?\n");
+					$userGuess = fgets(STDIN);
+				} elseif ($userGuess < $magicNumber) {
+					fwrite(STDOUT, "Too low! Higher!\n");
+					fwrite(STDOUT, "Can you guess the magic number?\n");
+					$userGuess = fgets(STDIN);
+				} 
+			}
 
-if ($userGuess == $magicNumber) {
-	fwrite(STDOUT, "You got it! You must be magic!\n");
-	if ($guessCount == 1) {
-		fwrite(STDOUT, "It only took you " . $guessCount . " time!\n");
+			if ($userGuess == $magicNumber) {
+				fwrite(STDOUT, "You got it! You must be magic!\n");
+				if ($guessCount == 1) {
+					fwrite(STDOUT, "It only took you " . $guessCount . " time!\n");
+				} else {
+					fwrite(STDOUT, "It only took you " . $guessCount . " times!\n");
+				}
+			}
+		} else {
+			echo("Your arguments need to be numeric. Try again.\n");
+		}
 	} else {
-		fwrite(STDOUT, "It only took you " . $guessCount . " times!\n");
+		echo("Your arguments need to be numeric. Try again.\n");
 	}
+} else {
+	echo("You need a total of three arguments. Only enter two after the file name.\n");
 };
