dabd700e514b15d845b1112bcf1d61b93b1ffca7
John Hernandez <johnhernandez@Johns-MacBook-Air.local>
2016-07-05 14:50:27 -0500

    This is the completed for each exercise.

--- foreach.php
+++ foreach.php
@@ -1,3 +1,24 @@
 <?php 
 
 $things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, (string) 11);
+
+foreach ($things as $thing) {
+	if (is_string($thing)) {
+		echo $thing . " is a string.\n";
+	} elseif (is_numeric($thing)) {
+		echo $thing . " is numeric.\n";
+	} elseif (is_null($thing)) {
+		echo $thing . " is null.\n";
+	} elseif (is_array($thing)) {
+		var_dump($thing);
+		foreach ($thing as $number) {
+			if (is_integer($number)) {
+				echo $number . " is an integer.\n";
+			}
+		}
+	} elseif (is_bool($thing)) {
+		var_dump($thing);
+	} else {
+		echo "I HAVE NO IDEA WHAT THIS IS.\n";
+	}
+};
