7fcbc2d63ad1d6a20ab98404c717d508e45168a9
John Hernandez <johnhernandez@Johns-MacBook-Air.local>
2016-07-05 09:57:25 -0500

    I added a count of guesses feature to the highlow game.

--- highlow.php
+++ highlow.php
@@ -6,8 +6,10 @@ $magicNumber = mt_rand(1,100);
 fwrite(STDOUT, "Can you guess the magic number?\n");
 
 $userGuess = fgets(STDIN);
+$guessCount = 0;
 
 while ($userGuess != $magicNumber) {
+	$guessCount++;
 	if ($userGuess > $magicNumber) {
 		fwrite(STDOUT, "Too high! Lower!\n");
 		fwrite(STDOUT, "Can you guess the magic number?\n");
@@ -21,4 +23,9 @@ while ($userGuess != $magicNumber) {
 
 if ($userGuess == $magicNumber) {
 	fwrite(STDOUT, "You got it! You must be magic!\n");
+	if ($guessCount == 1) {
+		fwrite(STDOUT, "It only took you " . $guessCount . " time!\n");
+	} else {
+		fwrite(STDOUT, "It only took you " . $guessCount . " times!\n");
+	}
 };
