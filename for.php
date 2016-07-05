7bfb42fad186e49ab7c52f3de2306d3f280527ff
John Hernandez <johnhernandez@Johns-MacBook-Air.local>
2016-07-05 14:28:09 -0500

    This commit includes a finished off fizzbuzz exercise and the beginning of a for each iteration exercise.

--- for.php
+++ for.php
@@ -1,14 +1,15 @@
 <?php
 
-fwrite(STDOUT, "Choose a starting number. ");
+fwrite(STDOUT, "Choose a starting number: ");
 $start = trim(fgets(STDIN));
-fwrite(STDOUT, "Choose an ending number. ");
+fwrite(STDOUT, "Choose an ending number: ");
 $end = trim(fgets(STDIN));
+fwrite(STDOUT, "Choose an increment: ");
+$increment = trim(fgets(STDIN));
 
 if (is_numeric($start) && is_numeric($end)) {
-	for ($i = $start; $i <= $end; $i++) {
+	for ($i = $start; $i <= $end; $i += $increment) {
 		echo $i . PHP_EOL;
-		`say "$i"`;
 	}
 };
 
