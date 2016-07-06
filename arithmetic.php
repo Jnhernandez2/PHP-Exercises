6e0d0806f49cd87157f1483c0fa0d60b6ac20cb2
John Hernandez <johnhernandez@Johns-MacBook-Air.local>
2016-07-06 11:48:12 -0500

    I've added a modulus function and called it.

--- arithmetic.php
+++ arithmetic.php
@@ -20,9 +20,14 @@ function divide($a, $b)
     return $a / $b;
 }
 
+function modulus($a, $b)
+{
+	return $a % $b;
+}
 // Add code to test your functions here
 
 echo add(12, 15), PHP_EOL;
 echo subtract(37, 15), PHP_EOL;
 echo multiply(73, 25), PHP_EOL;
 echo divide(36, 3), PHP_EOL;
+echo modulus(25, 8), PHP_EOL;
