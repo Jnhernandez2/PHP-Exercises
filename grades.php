7e24bdcc2392adfbf44041b81203abcdc814a82b
John Hernandez <johnhernandez@Johns-MacBook-Air.local>
2016-07-06 23:47:29 -0500

    I've done a whole lot with this php grades thing. It's still not working. Something is going on with my math in my calculate average function. I know it has something to do with my for each. Everthing else seems to work.

---
+++ grades.php
@@ -0,0 +1,51 @@
+<?php
+function confirm($message) {
+	fwrite(STDOUT, $message);
+	$confrimation = trim(fgets(STDIN));
+	if ($confrimation == '1') {
+		return true;
+	}
+
+	return false;
+}
+
+
+//var_dump(confirm("Is there another subject you'd like to add? Enter '1' for yes, or enter 0 for no: "));die;
+
+$subjects = [];
+
+do {
+	fwrite(STDOUT, "Please enter a subject: ");
+	$name = fgets(STDIN);
+	fwrite(STDOUT, "Please enter a grade: ");
+	$grade= fgets(STDIN);
+	$subjects[] = ['name' => $name, 'grade' => $grade];
+} while (confirm("Is there another subject you'd like to add? Enter '1' for yes, or enter 0 for no: "));
+
+var_dump($subjects);
+$actualGrade = [];
+foreach ($subjects as $array) {
+	$actualGrade[] = (int)$array['grade'];
+}
+var_dump($actualGrade);
+$average = 0;
+
+function calculateAverage($array) {
+	$total = 0;
+	foreach ($array as $number) {
+		$number += $number;
+	}
+	$total = $number;
+	echo $total, PHP_EOL;
+	$average = $total / count($number);
+	return $average;
+}	
+
+$getAverage = calculateAverage($actualGrade);
+echo "Student average is $getAverage\n";
+
+if ($getAverage >= 80) {
+	echo("You did great! You're an awesome student!\n");
+} else {
+	echo("Boo, you're terrible.\n");
+};
