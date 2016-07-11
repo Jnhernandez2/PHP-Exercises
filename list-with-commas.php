0f1c6db63ad5f2c12f93beffdabe5091df7ff193
John Hernandez <johnhernandez@Johns-MacBook-Air.local>
2016-07-11 11:20:02 -0500

    This is the complete version of my implode/explode exercise.

--- list-with-commas.php
+++ list-with-commas.php
@@ -1,2 +1,20 @@
 <?php 
 
+$famousFakePhysicists = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mallory, Bruce Banner, Tony Stark';
+
+
+function humanizePhysicists($string, $sort = false) {
+	$arrayFromString = explode(', ', $string);
+	if ($sort) {
+		sort($arrayFromString);
+	}
+	$lastItem = array_pop($arrayFromString);
+	array_push($arrayFromString, "and ");
+	$incompleteString = implode(', ', $arrayFromString);
+	$completeString = $incompleteString . $lastItem;
+	return $completeString;
+}
+
+$famousPhysicists = humanizePhysicists($famousFakePhysicists, true);
+
+echo "Some of the most famous fictional theoretical physicists are $famousPhysicists.\n";
